8b7b14d18e34245c2c95ae69538dc77a27c7fb72
Nasywa <nasywaraihanah@gmail.com>
2022-06-16 22:52:09 +0700

    terminal

---
+++ kuliah/pertemuan12/latihan1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+<body>
+  <h3>Daftar Mahasiswa</h3>
+  <title border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Nama</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="img/hatice-yardim-Bpso4XgDJ5I-unsplash.jpg"></td>
+      <td>3411211110</td>
+      <td>Nasywa</td>
+      <td>nasywaraihanah@gmail.com</td>
+      <td>Informatika</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+  </title>
+</body>
+</html>
